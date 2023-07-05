94cdd3c45e426d27145fbfbb2b1f3803528d39c6
theocnjd <58237191+theocnjd@users.noreply.github.com>
2023-07-05 01:10:35 +0100

    Complete member interface and home banner

--- CiSApp/Site.Master.designer.cs
+++ CiSApp/Site.Master.designer.cs
@@ -78,6 +78,15 @@ namespace CiS
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvsidebarToggle;
 
         /// <summary>
+        /// banner control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl banner;
+
+        /// <summary>
         /// masterTopNavBar control.
         /// </summary>
         /// <remarks>
